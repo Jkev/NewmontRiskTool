1943e2e1ff23b8ef49505f20f82df1a4f3cda61e
Jkev <endlezzjkev@gmail.com>
2021-06-01 14:58:05 -0500

    Updates

---
+++ precache-manifest.50580256d8fb897cbc5e34b03064606d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2e19131b4466689ef20d3eb83a32a3",
+    "url": "/NewmontRiskTool/index.html"
+  },
+  {
+    "revision": "2cbaaaa82ba3a3724510",
+    "url": "/NewmontRiskTool/static/css/2.4d337f89.chunk.css"
+  },
+  {
+    "revision": "66676bbc5ae2653d986c",
+    "url": "/NewmontRiskTool/static/css/main.7577d335.chunk.css"
+  },
+  {
+    "revision": "2cbaaaa82ba3a3724510",
+    "url": "/NewmontRiskTool/static/js/2.2dda89f3.chunk.js"
+  },
+  {
+    "revision": "892304f0dc2fa332c91d42c7da7402f8",
+    "url": "/NewmontRiskTool/static/js/2.2dda89f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66676bbc5ae2653d986c",
+    "url": "/NewmontRiskTool/static/js/main.44a2f297.chunk.js"
+  },
+  {
+    "revision": "9d0a942db8f40bda5270",
+    "url": "/NewmontRiskTool/static/js/runtime-main.bc731b40.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/NewmontRiskTool/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/NewmontRiskTool/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/NewmontRiskTool/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/NewmontRiskTool/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/NewmontRiskTool/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
